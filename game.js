13967a5cf0e40974e435746c3419cb2e6c8efcf5
kpmarz <you@example.com>
2018-11-13 18:32:49 -0500

    Adding Files

---
+++ game.js
@@ -0,0 +1,14 @@
+var Deck = require('./Deck.js');
+
+// The Duck Tales guys are going to the casino and 
+// they want to simulate 20 games of black jack.  Each
+// players starts with 200 dollars.  Each game is $15.  If
+// they lose all their money they can no longer play.  Simulate
+// the outcome.  At the end output the player and how much
+// they each won / lost.
+
+// RULES
+// Number of Decks between 1-8
+// Dealer Hits on 16 and Below
+
+//Simulation Data 
