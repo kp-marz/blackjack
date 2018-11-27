02dfc92a0c51f33b0265d05b05a06eda00f8e717
kpmarz <you@example.com>
2018-11-27 18:35:56 -0500

    1.2

--- game.js
+++ game.js
@@ -12,36 +12,12 @@ var Deck = require('./Deck.js');
 // Dealer Hits on 16 and Below
 
 //Simulation Data 
-const _ante = 15, _maxRounds = 20, startingMoneyAmount = 200;
+const _ante = 15, _maxRounds = 20, _startingMoneyAmount = 200;
 var _numberOfDecks = 1, _deck, _round = 1;
-var _players = [new Player('Lock', 200),
-                new Player('Shock', 200),
-                new Player('Barrel', 200),
-                new Player('Oggie', 200)];
-var _dealer = new Dealer('Dealer');
+var _players = [new Player('Lock', _startingMoneyAmount),
+                new Player('Shock', _startingMoneyAmount),
+                new Player('Barrel', _startingMoneyAmount),
+                new Player('Oggie', _startingMoneyAmount)];
+//var _dealer = new Dealer('Dealer');
 
-loop for 20 rounds {
-
-    create deck of Decks
-
-    players put in their startingMoneyAmount
-
-    dealer shuffles deck
-
-    dealer deals the cards to players and dealer
-
-    dealer is showing one card up (the second card in his/her hand)
-
-    players cards are up
-
-    player 1 will evalute and perform hit, stay, split, double down
-
-    eventually must stay or bust
-
-    dealer will evaluate then stay or hit
-
-    reconcile - payout or collect _ante
-
-}
-
-Print results
+console.log(_players[1].split(0,_ante));
--- players.js
+++ players.js
@@ -1,22 +1,38 @@
-var players =  [new Player('Lock', 200),
-new Player('Shock', 200),
-new Player('Barrel', 200),
-new Player('Oggie', 200)];
-   
-var _dealer = new Dealer('Dealer');
+function Player(name, money){
+    this.name = name,
+    this.money = money,
+    this.bettingBox = 0,
+    this.hasInsurance = false,
+    this.isDoubleDown = false,
+    this.hands = [[]],
+    this.hit =  function(hand,card){
+        this.hands[hand].push(card);
+    },
+    this.stay = function(){
+         
+    },
+    this.anteUp = function(ante){
+        this.bettingBox = this.bettingBox + ante;
+        this.money = this.money - ante;
+    },
+    this.evaluate = function(){
+        console.log('Not Implemented');
+    },
+    this.split = function(hand, ante){
+        this.anteUp(ante);
+        this.hands.push([]);
+        var secondCard = this.hands[hand].pop();
+        this.hands[hand+1].push(secondCard);
+    },
+    this.doubleDown = function(hand, card, ante){
+        this.anteUp(ante);
+        this.hands[hand].push(card);
+        this.isDoubleDown = true;
+    },
+    this.insurance = function(ante){
+        this.anteUp(ante);
+        this.hasInsurance = true;
+    }
+}
 
-function Player(name, money, hand){
-    this.name = name;
-    this.money = money;
-    this.hand = [];
-};
-
-let player01 = new Player('Lock', 200);
-let player02 = new Player('Shock', 200);
-let player03 = new Player('Barrel', 200);
-let player04 = new Player('Ogie', 200);
-
-function Dealer(name, hand){
-    this.name = name;
-    this.hand = [];
-};
+module.exports = Player;
